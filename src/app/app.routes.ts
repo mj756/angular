ccfa158d2532d8987924e3367499d6892652a10e
Milan Joshi <82023102+mj756@users.noreply.github.com>
2025-07-24 07:27:11 +0530

    latest push

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "login",
+    redirectTo: "/login",
+    loadComponent: () => import("./pages/login/login").then((m) => m.Login),
+  },
+  {
+    path: "dashboard",
+    redirectTo: "/dashboard",
+    loadComponent: () =>
+      import("./pages/dashboard/dashboard").then((m) => m.Dashboard),
+  },
+  {
+    path: "signup",
+    redirectTo: "/register",
+    loadComponent: () =>
+      import("./pages/dashboard/dashboard").then((m) => m.Dashboard),
+  },
+];
